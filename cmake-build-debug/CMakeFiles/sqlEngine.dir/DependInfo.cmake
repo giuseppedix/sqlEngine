70c972ed5add01d2541db2581ccc5a66b2aca1f7
RossellaYlw <rossella.centonze@live.it>
2020-05-18 19:46:23 +0200

    work in progress

---
+++ cmake-build-debug/CMakeFiles/sqlEngine.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rossellacentonze/Desktop/Prog/sqlEngine/Parser/Parser.cpp" "/Users/rossellacentonze/Desktop/Prog/sqlEngine/cmake-build-debug/CMakeFiles/sqlEngine.dir/Parser/Parser.cpp.o"
+  "/Users/rossellacentonze/Desktop/Prog/sqlEngine/Table/Column.cpp" "/Users/rossellacentonze/Desktop/Prog/sqlEngine/cmake-build-debug/CMakeFiles/sqlEngine.dir/Table/Column.cpp.o"
+  "/Users/rossellacentonze/Desktop/Prog/sqlEngine/Table/Row.cpp" "/Users/rossellacentonze/Desktop/Prog/sqlEngine/cmake-build-debug/CMakeFiles/sqlEngine.dir/Table/Row.cpp.o"
+  "/Users/rossellacentonze/Desktop/Prog/sqlEngine/Table/RowElement.cpp" "/Users/rossellacentonze/Desktop/Prog/sqlEngine/cmake-build-debug/CMakeFiles/sqlEngine.dir/Table/RowElement.cpp.o"
+  "/Users/rossellacentonze/Desktop/Prog/sqlEngine/Table/Table.cpp" "/Users/rossellacentonze/Desktop/Prog/sqlEngine/cmake-build-debug/CMakeFiles/sqlEngine.dir/Table/Table.cpp.o"
+  "/Users/rossellacentonze/Desktop/Prog/sqlEngine/main.cpp" "/Users/rossellacentonze/Desktop/Prog/sqlEngine/cmake-build-debug/CMakeFiles/sqlEngine.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/sqlEngine.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sqlEngine.dir/Parser/Parser.cpp.o"
+  "CMakeFiles/sqlEngine.dir/Table/Column.cpp.o"
+  "CMakeFiles/sqlEngine.dir/Table/Row.cpp.o"
+  "CMakeFiles/sqlEngine.dir/Table/RowElement.cpp.o"
+  "CMakeFiles/sqlEngine.dir/Table/Table.cpp.o"
+  "CMakeFiles/sqlEngine.dir/main.cpp.o"
+  "sqlEngine"
+  "sqlEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sqlEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
